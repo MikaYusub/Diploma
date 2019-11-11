88f977e2f50ea43d5df1bbe6a53ea0ccc4f5f0b5
Mikayil <mikayil.yusubov@gmail.com>
2019-11-11 14:17:22 +0400

    ----

--- Diploma/.idea/DiplomaDirectProblem.py
+++ Diploma/.idea/DiplomaDirectProblem.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from numpy.linalg import inv
 import matplotlib.animation as animation
 
-eps = 0.05
+eps = 0.1
 M = 100
 N = 150
 u_left = -8
@@ -12,8 +12,6 @@ a = 0
 b = 1
 T = 0.3
 t_0 = 0
-tau = (T - t_0) / M
-h = (b - a) / N
 q = []
 
 t = np.linspace(t_0, T, M + 1)
@@ -57,23 +55,25 @@ def func_y(u):
         f_y.itemset((n, n + 1),
                     (2 * eps / (x[n + 1] - x[n - 1])) * (1 / (x[n + 1] - x[n])) + u[n] / (x[n + 1] - x[n - 1]))
 
-    f_y.itemset((-1, -1), (-2 * eps / (x[N] - x[N - 2])) * (1 / (x[N] - x[N - 1]) + 1 / (x[N - 1] - x[N - 2])) +
+    f_y.itemset((N - 2, N - 2), (-2 * eps / (x[N] - x[N - 2])) * (1 / (x[N] - x[N - 1]) + 1 / (x[N - 1] - x[N - 2])) +
                 (u_right - u[N - 2]) / (x[N] - x[N - 2]) - q[N - 1])
-    return f_y # Тут всё правильно
+    return f_y  # Тут всё правильно
 
 
+y = np.zeros((M + 1, N - 1))
 u = np.zeros((M + 1, N + 1))
 for n in range(N + 1):
     u[0, n] = u_init(x[n])
+y[0, :] = u[0, 1:N]
 
 for m in range(M):
     tmp = ((1 + 1j) * (t[m + 1] - t[m]) / 2)
     tmp1 = np.eye(N - 1) - tmp * (func_y(u[m, :]))
-    w_1 = np.dot(inv(tmp1), func(u[m, :]))
-    # w_1 = inv(tmp1) * func(u[m, :])
+    w_1 = np.matmul(inv(tmp1), func(u[m, :]))
     tmp2 = (t[m + 1] - t[m]) * w_1.real
-    u[m + 1, 1:N] = u[m, 1:N] + np.transpose(tmp2)
+    y[m + 1] = y[m] + np.transpose(tmp2)
     u[m + 1, 0] = u_left
+    u[m + 1, 1:N] = y[m + 1, :]
     u[m + 1, N] = u_right
 
 plt.axis([a, b, -9, 6])
--- Diploma/.idea/DirectProblemTest.py
+++ Diploma/.idea/DirectProblemTest.py
@@ -4,8 +4,8 @@ from numpy.linalg import inv
 import matplotlib.animation as animation
 
 eps = 0.1
-M = 50
-N = 30
+M = 100
+N = 50
 u_left = 0
 u_right = 0
 a = 0
@@ -17,22 +17,26 @@ x = np.linspace(a, b, N + 1)
 
 
 def tmp_f(x, t):
-    return -eps * (1 - 2 * t) * np.sin(x) + 2 * np.sin(x) \
+    s = -eps * (1 - 2 * t) * np.sin(x) + 2 * np.sin(x) \
            + ((1 - 2 * t) ** 2) * np.sin(x) * np.cos(x) - \
            np.sin(3 * np.pi * x) * (1 - 2 * t) * np.sin(x)
+
+    return s
 
 def q(x):
     return np.sin(3 * x)
 
+
 def u_func(x, t):
     return (1 - 2 * t) * np.sin(x)
+
 
 def func(u, t):
     f = np.zeros((N - 1, 1), dtype='float64')
     f.itemset(0, (2 * eps / (x[2] - x[0])) * ((u[2] - u[1]) / (x[2] - x[1]) - (u[1] - u_left) / (x[1] - x[0])) + \
               (u[1] * (u[2] - u_left)) / (x[2] - x[0]) - (u[1] * q(x[1])) - tmp_f(x[0], t)
               )
-    for n in range(1, N-1):
+    for n in range(1, N - 1):
         kk = 2 * eps / (x[n + 1] - x[n - 1])
         k1 = ((u[n + 1] - u[n]) / (x[n + 1] - x[n])) - ((u[n] - u[n - 1]) / (x[n] - x[n - 1]))
         k2 = u[n] * ((u[n + 1] - u[n - 1]) / (x[n + 1] - x[n - 1]))
@@ -42,7 +46,6 @@ def func(u, t):
     t3 = u[N - 1] * ((u_right - u[N - 2]) / (x[N] - x[N - 2])) - (u[N - 1] * q(x[N - 1]))
     f.itemset(-1, t1 * t2 + t3 - tmp_f(x[N - 1], t))
     return f
-
 
 def func_y(u):
     f_y = np.zeros((N - 1, N - 1), dtype='float64')
@@ -68,26 +71,17 @@ for n in range(N + 1):
     u[0, n] = u_func(x[n], t_0)
 y[0, :] = u[0, 1:N]
 
-# for m in range(M):
-#     tmp = ((1 + 1j) * (t[m + 1] - t[m]) / 2)
-#     tmp1 = np.eye(N - 1) - tmp * (func_y(u[m, :]))
-#     w_1 = np.dot(inv(tmp1), func(u[m, :], (t[m] + t[m + 1]) / 2))
-#     tmp2 = (t[m + 1] - t[m]) * w_1.real
-#     u[m + 1, 0] = u_left
-#     u[m + 1, N] = u_right
-#     u[m + 1, 1:N] = u[m, 1:N] + np.transpose(tmp2)
-#     print(func(u[m, :],t[m]))
 for m in range(M):
-    tmp = ((1+1j)*(t[m+1]-t[m])/2)
-    tmp1 = np.eye(N - 1) - tmp * (func_y(y[m, :]))
-    w_1 = np.dot(inv(tmp1), func(y[m, :], (t[m] + t[m + 1]) / 2))
+    tmp = ((1 + 1j) * (t[m + 1] - t[m]) / 2)
+    tmp1 = np.eye(N - 1) - tmp * (func_y(u[m, :]))
+    w_1 = np.dot(inv(tmp1), func(u[m, :], (t[m] + t[m + 1]) / 2))
     tmp2 = (t[m + 1] - t[m]) * w_1.real
     y[m + 1] = y[m] + np.transpose(tmp2)
-    u[m+1, 0] = u_left
-    u[m+1, 1:N] = y[m+1, :]
-    u[m+1, N] = u_right
+    u[m + 1, 0] = u_left
+    u[m + 1, 1:N] = y[m + 1]
+    u[m + 1, N] = u_right
 
-plt.axis([0, 1, -600, 600])
+plt.axis([0, 1, -100, 100])
 plt.plot(t, u);
 plt.show()
 
@@ -95,7 +89,6 @@ fig2 = plt.figure(facecolor='white')
 ax = plt.axes(xlim=(0, 1), ylim=(-500, 500))
 line, = ax.plot([], [], lw=3)
 
-#
 # def redraw(i):
 #     x = t[i]
 #     y = func(u[i, :],t[i])
