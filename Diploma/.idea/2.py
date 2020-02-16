25c5b76d3f13000c2b4185815cfce3c58e59e195
Mikayil <mikayil.yusubov@gmail.com>
2020-02-16 23:42:32 +0400

    ----

--- Diploma/.idea/2.py
+++ Diploma/.idea/2.py
@@ -1,76 +1,61 @@
-import numpy as np
-import matplotlib.pyplot as plt
+def direct_problem(eps, M, N, u_left, u_right, t, x, q, h):
+    def func(y, t, x, q):
+        f = np.zeros((N - 1, 1))
+        f.itemset(0,
+                  (eps * (y[1] - 2 * y[0] + u_left) / h ** 2)
+                  + (y[0] * (y[1] - u_left) / (2 * h))
+                  - y[0] * q[1]
+                  )
+        for n in range(1, N - 2):
+            f.itemset(n,
+                      (eps * (y[n + 1] - 2 * y[n] + y[n - 1]) / h ** 2)
+                      + (y[n] * (y[n + 1] - y[n - 1]) / (2 * h))
+                      - y[n] * q[n + 1]
+                      )
+        f.itemset(N - 2,
+                  (eps * (u_right - 2 * y[N - 2] + y[N - 3]) / h ** 2)
+                  + (y[N - 2] * (u_right - y[N - 3]) / (2 * h))
+                  - y[N - 2] * q[N - 1]
+                  )
+        return f
 
-eps = 0.1
-dBdu = -1
-M = 50
-N = 30
-u_left = 0
-u_right = 0
-a = 0
-b = np.pi
-T = 1
-t_0 = 0
-tau = (T - t_0) / M
-h = (b - a) / N
-t = np.linspace(t_0, T, M + 1)
-x = np.linspace(a, b, N + 1)
+    def func_y(y, q):
+        f_y = np.zeros((N - 1, N - 1))
+        f_y.itemset((0, 0),
+                    (-2 * eps / h ** 2)
+                    + ((y[1] - u_left) / (2 * h))
+                    - q[1])
+        for n in range(1, N - 1):
+            f_y.itemset((n, n - 1),
+                        eps / h ** 2 - y[n] / (2 * h))
+        for n in range(1, N - 2):
+            f_y.itemset((n, n),
+                        -2 * eps / h ** 2
+                        + ((y[n + 1] - y[n - 1]) / (2 * h)) - q[n + 1])
+        for n in range(0, N - 2):
+            f_y.itemset((n, n + 1),
+                        eps / h ** 2 + y[n] / (2 * h))
 
+        f_y.itemset((-1, -1),
+                    -2 * eps / h ** 2
+                    + ((u_right - y[N - 3]) / (2 * h))
+                    - q[N - 1])
+        return f_y
 
-def u_init(x):
-    return (((2 - x) - (x + 4) * np.exp((3 * x - 0.75) / eps)) / (1 + np.exp((3 * x - 0.75) / eps))) + 3
-
-
-def B(u, x):
-    return -u
-
-
-def func(y, t, x, h, N, u_left, u_right):
-    f = []
-    for i in range(N - 1):
-        f.append(i * 0)
-
-    f[0] = (eps * (y[1] - 2 * y[0] + u_left) / h ** 2) - y[0] * (y[1] - u_left) / (2 * h) + B(y[0], x[1])
-    for n in range(1, N - 1, 1):
-        f[n] = eps * (y[n + 1] - 2 * y[n] + y[n - 1]) / h ** 2 \
-               - y[n] * (y[n + 1] - y[n - 1]) / (2 * h) + B(y[n], x[n + 1])
-
-    f[N - 1] = eps * (u_right - 2 * y[N - 1] + y[N - 2]) / h ** 2 - y[N - 1] * (u_right - y[N - 2]) / (2 * h) + B(
-        y[N - 1], x[N])
-
-
-def func_y(y, t, x, h, N, u_left, u_right):
-    f_y = np.zeros((N - 1, N - 1))
-    f_y[0, 0] = eps * (-2 / h * h) - (y[2] - u_left) / (2 * h) + dBdu
-    for i in range(1, N - 2):
-        f_y[i, i - 1] = eps * (1 / h ** 2) + y[i] / (2 * h)
-    for i in range(1, N - 3):
-        f_y[i, i] = eps * (-2 / h ** 2) - (y[i + 1] - y[i - 1]) / (2 * h) + dBdu
-    for i in range(N - 3):
-        f_y[i, i + 1] = eps * (1 / h ** 2) - y[i] / (2 * h)
-    f_y[N - 2, N - 2] = eps * (-2 / h ** 2) - (u_right - y[N - 2]) / (2 * h) + dBdu
-
-
-def example3(a, b, N, M, t_0, T, u_left, u_right, u_init):
     u = np.zeros((M + 1, N + 1))
     y = np.zeros((M + 1, N - 1))
     for n in range(N + 1):
         u[0, n] = u_init(x[n])
-        # u[1, :] = u_init(x)
-        y[1, :] = u[1, 2:N]
+    y[0, :] = u[0, 1:N]
+
     for m in range(M):
-        w_1 = (np.eye(N - 1)
-               - (1 + 1j) / 2 * (t[m + 1] - t[m]) * f_y(y[m, :], t[m],
-                                                        x, h, N, u_left, u_right, ))
-        y[m + 1, :] = y[m, :] + (t[m + 1] - t[m]) * w_1.real
-        u[m + 1, 1] = u_left
-        u[m + 1, 2:N] = y[m + 1, :]
-        u[m + 1, N + 1] = u_right
+        tmp = ((1 + 1j) * (t[m + 1] - t[m]) / 2)
+        tmp1 = np.eye(N - 1) - tmp * (func_y(y[m, :], q))
+        w_1 = solve(tmp1, func(y[m, :], (t[m] + t[m + 1]) / 2, x, q))
+        tmp2 = (t[m + 1] - t[m]) * w_1.real
+        y[m + 1] = y[m] + np.transpose(tmp2)
+        u[m + 1, 1:N] = y[m + 1]
+        u[m + 1, 0] = u_left
+        u[m + 1, N] = u_right
 
-    plt.plot(t, u, linewidth=3.0)
-    plt.xlabel('t')
-    plt.ylabel('u')
-    plt.show()
-
-
-example3(0, 1, 100, 200, 0, 0.3, 4, -8, u_init(x))
+    return u
--- Diploma/.idea/DiplomaDirectProblem.py
+++ Diploma/.idea/DiplomaDirectProblem.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.linalg import inv,solve;
+from numpy.linalg import inv, solve;
 import matplotlib.animation as animation
 
 plt.rcParams['animation.ffmpeg_path'] = r'C:\FFmpeg\bin\ffmpeg'
 
 eps = 0.05
-M = 300
-N = 300
+M = 200
+N = 200
 u_left = -8
 u_right = 4
 a = 0
@@ -18,46 +18,46 @@ h = (b - a) / N
 t = np.linspace(t_0, T, M + 1)
 x = np.linspace(a, b, N + 1)
 init_q = [];
-q = np.zeros(N+1)
+q = np.zeros(N + 1)
+
 
 def q_init(x):
-    return 2*x - 1 +2*np.sin(5*x*np.pi)+0.35
+    return 2 * x - 1 + 2 * np.sin(5 * x * np.pi) + 0.35
     # return np.sin(3 * x * np.pi)
+
 
 for n in range(0, N + 1):
     init_q.append(q_init(x[n]))
 
+
 def u_init(x):
-    return (x**2 - x - 2) \
-           - 6* np.tanh((-3*x + 0.75)/eps)
+    return (x ** 2 - x - 2) \
+           - 6 * np.tanh((-3 * x + 0.75) / eps)
 
-def direct_problem(eps,M,N,a,b,u_left,u_right,T,t_0,t,x,q,h):
 
-    def func(y,t,x,q):
+def direct_problem(eps, M, N, u_left, u_right, t, x, q, h):
+    def func(y, t, x, q):
         f = np.zeros((N - 1, 1))
         f.itemset(0,
                   (eps * (y[1] - 2 * y[0] + u_left) / h ** 2)
                   + (y[0] * (y[1] - u_left) / (2 * h))
                   - y[0] * q[1]
-                  # - tmp_f(x[1], t)
                   )
         for n in range(1, N - 2):
             f.itemset(n,
                       (eps * (y[n + 1] - 2 * y[n] + y[n - 1]) / h ** 2)
                       + (y[n] * (y[n + 1] - y[n - 1]) / (2 * h))
                       - y[n] * q[n + 1]
-                      # - tmp_f(x[n + 1], t)
                       )
         f.itemset(N - 2,
                   (eps * (u_right - 2 * y[N - 2] + y[N - 3]) / h ** 2)
                   + (y[N - 2] * (u_right - y[N - 3]) / (2 * h))
                   - y[N - 2] * q[N - 1]
-                  # - tmp_f(x[N - 1], t)
                   )
         return f
 
-    def func_y(y,q):
-        f_y = np.zeros((N - 1, N - 1), dtype='float64')
+    def func_y(y, q):
+        f_y = np.zeros((N - 1, N - 1))
         f_y.itemset((0, 0),
                     (-2 * eps / h ** 2)
                     + ((y[1] - u_left) / (2 * h))
@@ -88,32 +88,15 @@ def direct_problem(eps,M,N,a,b,u_left,u_right,T,t_0,t,x,q,h):
     for m in range(M):
         tmp = ((1 + 1j) * (t[m + 1] - t[m]) / 2)
         tmp1 = np.eye(N - 1) - tmp * (func_y(y[m, :], q))
-        # w_1 = np.dot(inv(tmp1), func(y[m, :], (t[m] + t[m + 1]) / 2, x, q)).real
-        w_1 = solve(tmp1,func(y[m, :], (t[m] + t[m + 1]) / 2, x, q))
+        w_1 = solve(tmp1, func(y[m, :], (t[m] + t[m + 1]) / 2, x, q))
         tmp2 = (t[m + 1] - t[m]) * w_1.real
         y[m + 1] = y[m] + np.transpose(tmp2)
         u[m + 1, 1:N] = y[m + 1]
         u[m + 1, 0] = u_left
         u[m + 1, N] = u_right
 
-
-    # fig2 = plt.figure(facecolor='white')
-    # ax = plt.axes(xlim=(a, b), ylim=(-9, 5))
-    # line, = ax.plot([], [], lw=1, color='red')
-    # line2, = ax.plot([], [], lw=1, color='green')
-    #
-    # def animate(i):
-    #     line.set_xdata(x)
-    #     line.set_ydata(u[i, :])
-    #     line2.set_xdata(x)
-    #     line2.set_ydata(u_init(x))
-    #     return line, line2
-    #
-    # anim = animation.FuncAnimation(fig2, animate, frames=M + 1, interval=50, blit= True)
-    # FFwriter = animation.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
-    # anim.save(r'C:\Users\FS\Desktop\Main Mission\Direct_problem_solution.mp4', writer=FFwriter)
-    # plt.show()
     return u
+
 
 def conjucate_problem(eps, M, N, t, x, q, h, u, f_obs):
     def func_psi(y, u, t, q):
@@ -126,12 +109,12 @@ def conjucate_problem(eps, M, N, t, x, q, h, u, f_obs):
         for n in range(1, N - 2):
             f.itemset(n,
                       (-eps * (y[n + 1] - 2 * y[n] + y[n - 1]) / h ** 2)
-                      + (u[n+1] * (y[n + 1] - y[n - 1]) / (2 * h))
-                      + y[n] * q[n+1]
+                      + (u[n + 1] * (y[n + 1] - y[n - 1]) / (2 * h))
+                      + y[n] * q[n + 1]
                       )
         f.itemset(N - 2,
                   (-eps * (-2 * y[N - 2] + y[N - 3]) / h ** 2)
-                  - (u[N - 1] * y[N - 3] / (2 * h))
+                  + (u[N - 1] * y[N - 3] / (2 * h))
                   + y[N - 2] * q[N - 1]
                   )
         return f
@@ -139,11 +122,11 @@ def conjucate_problem(eps, M, N, t, x, q, h, u, f_obs):
     def func_y_psi(u, q):
         f_y = np.zeros((N - 1, N - 1), dtype='float64')
         for n in range(1, N - 1):
-            f_y.itemset((n, n - 1), -eps / h ** 2 - u[n+1] / (2 * h))
+            f_y.itemset((n, n - 1), -eps / h ** 2 - u[n + 1] / (2 * h))
         for n in range(0, N - 1):
-            f_y.itemset((n, n), 2 * eps / h ** 2 + q[n+1])
+            f_y.itemset((n, n), 2 * eps / h ** 2 + q[n + 1])
         for n in range(0, N - 2):
-            f_y.itemset((n, n + 1), (-eps / h ** 2) + (u[n+1] / (2 * h)))
+            f_y.itemset((n, n + 1), (-eps / h ** 2) + (u[n + 1] / (2 * h)))
         return f_y
 
     psi = np.zeros((M + 1, N + 1))
@@ -162,25 +145,25 @@ def conjucate_problem(eps, M, N, t, x, q, h, u, f_obs):
     psi[:, N] = 0
 
     fig2 = plt.figure(facecolor='white')
-    ax = plt.axes(xlim=(-0.1, 1.1), ylim=(-8, 0.5))
+    ax = plt.axes(xlim=(0, 1), ylim=(-13, 3))
     line, = ax.plot([], [], lw=1, color='red')
-    line2, = ax.plot([], [], lw=1, color='green')
+    line2, = ax.plot([], [], lw=1, color='green',linestyle='--')
 
     def animate(i):
         line.set_xdata(x)
         line.set_ydata(psi[i, :])
-        # line2.set_xdata(x)
-        # line2.set_ydata(psi_model(x, t[i]))
+        line2.set_xdata(x)
+        line2.set_ydata(psi[M,:])
         return line
 
-    anim = animation.FuncAnimation(fig2, animate, frames= 1 + M, interval=50)
-    FFwriter = animation.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
-    anim.save(r'C:\Users\FS\Desktop\Main Mission\ConjTest.mp4', writer=FFwriter)
+    anim = animation.FuncAnimation(fig2, animate, frames=1 + M, interval=50)
+    # FFwriter = animation.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264'])
+    # anim.save(r'C:\Users\FS\Desktop\Main Mission\ConjTest.mp4', writer=FFwriter)
     plt.show()
     return psi
 
 
-U = direct_problem(eps,M,N,a,b,u_left,u_right,T,t_0,t,x,init_q,h)
+U = direct_problem(eps, M, N, u_left, u_right, t, x, init_q, h)
 
-u1 = direct_problem(eps,M,N,a,b,u_left,u_right,T,t_0,t,x,q,h)
-conjucate_problem(eps,M,N,t,x,q,h,u1,f_obs= U[M,:])
+u1 = direct_problem(eps, M, N, u_left, u_right, t, x, q, h)
+conjucate_problem(eps, M, N, t, x, q, h, u1, f_obs=U[M, :])
